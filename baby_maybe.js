44a6f047d8432b625420a575c7e48b7742e51c76
JSteaze <jaredcmail@gmail.com>
2015-08-18 15:52:37 -0600

    harvester spawn
    added harvester spawning automatically,

--- baby_maybe.js
+++ baby_maybe.js
@@ -33,6 +33,56 @@ var harvester = require('harvester');
 var builder = require('builder');
 var guard = require('guard');
 
+var harvester_count = 0;
+var builder_count = 0;
+var guard_count = 0;
+var ranged_guard_count = 0;
+
+for (var i in Game.creeps)
+{
+	var creep = Game.creeps[i];
+
+	if (creep.memory.role == 'harvester')
+	{
+		harvester_count = harvester_count + 1;
+	}
+
+	if (creep.memory.role == 'builder')
+	{
+		builder_count = builder_count + 1;
+	}
+
+	if (creep.memory.role == 'guard')
+	{
+		guard_count = guard_count + 1;
+	}
+
+	if (creep.memory.role == 'ranged_guard')
+	{
+		ranged_guard_count = ranged_guard_count + 1;
+	}
+}
+
+if (harvester_count <= (builder_count || guard_count || ranged_guard_count))
+{
+	//var possible_spawn = room.find(FIND_MY_SPAWNS);
+
+	for (var i in Game.spawns)
+	{
+		var possible_spawn = Game.spawns[i];
+		if (possible_spawn.canCreateCreep( [WORK, MOVE, CARRY] , 'Harvester' + (harvester_count + 1)) == OK)
+		{
+			possible_spawn.createCreep( [WORK, MOVE, CARRY] , 'Harvester' + (harvester_count + 1));
+		}
+	}
+}
+
+
+// if (Game.creeps.room == 0)
+// {
+// 	Game.spawns.Spawn1.createCreep( [WORK, WORK, CARRY, MOVE], 'Harvester1' );
+// }
+
 for(var name in Game.creeps) 
 {
 	var creep = Game.creeps[name];
