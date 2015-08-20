2c4e9be08a2ddba738a3a039847e6fffec07ab1a
JSteaze <jaredcmail@gmail.com>
2015-08-20 16:52:47 -0600

    population
    population manager now works.  Next work on spawn order and guards not
    attacking alone.

--- baby_maybe.js
+++ baby_maybe.js
@@ -7,86 +7,9 @@ var harvester = require('harvester');
 var builder = require('builder');
 var guard = require('guard');
 var ranged_guard = require('ranged_guard');
-//var population_manager = require('population_manager')();
+var population_manager = require('population_manager');
 
-//population_manager.check_population();
-//population_manager.balance_population();
-
-for (var i in Game.creeps)
-{
-	var creep = Game.creeps[i];
-
-	if (creep.memory.role == 'harvester')
-	{
-		harvester_count = harvester_count + 1;
-	}
-
-	if (creep.memory.role == 'builder')
-	{
-		builder_count = builder_count + 1;
-	}
-
-	if (creep.memory.role == 'guard')
-	{
-		guard_count = guard_count + 1;
-	}
-
-	if (creep.memory.role == 'ranged_guard')
-	{
-		ranged_guard_count = ranged_guard_count + 1;
-	}
-}
-
-
-if (harvester_count <= (builder_count || guard_count || ranged_guard_count))
-{
-	for (var i in Game.spawns)
-	{
-		var possible_spawn = Game.spawns[i];
-		if (possible_spawn.canCreateCreep( [WORK, MOVE, CARRY] , undefined, {role: 'harvester'}) == OK)
-		{
-			possible_spawn.createCreep( [WORK, MOVE, CARRY] , undefined, {role: 'harvester'});
-		}
-	}
-}
-
-if (guard_count < (builder_count || builder_count || ranged_guard_count))
-{
-	for (var i in Game.spawns)
-	{
-		var possible_spawn = Game.spawns[i];
-		if (possible_spawn.canCreateCreep( [TOUGH, ATTACK, MOVE, CARRY] , undefined, {role: 'guard'}) == OK)
-		{
-			possible_spawn.createCreep( [WORK, MOVE, CARRY] , undefined, {role: 'guard'});
-		}
-	}
-}
-
-if (builder_count < (harvester_count || guard_count || ranged_guard_count))
-{
-	for (var i in Game.spawns)
-	{
-		var possible_spawn = Game.spawns[i];
-		if (possible_spawn.canCreateCreep( [WORK, WORK, MOVE, CARRY] , undefined, {role: 'builder'}) == OK)
-		{
-			possible_spawn.createCreep( [WORK, WORK, MOVE, CARRY] , undefined, {role: 'builder'});
-		}
-	}
-}
-
-if (ranged_guard_count < (builder_count || guard_count || harvester_count))
-{
-	for (var i in Game.spawns)
-	{
-		var possible_spawn = Game.spawns[i];
-		if (possible_spawn.canCreateCreep( [RANGED_ATTACK, MOVE, MOVE, TOUGH] , undefined, {role: 'ranged_guard'}) == OK)
-		{
-			possible_spawn.createCreep( [RANGED_ATTACK, MOVE, MOVE, TOUGH] , undefined, {role: 'ranged_guard'});
-		}
-	}
-}
-
-
+population_manager();
 
 
 for(var name in Game.creeps) 
--- population_manager.js
+++ population_manager.js
@@ -1,5 +1,3 @@
-
-
 var harvester_count = 0;
 var builder_count = 0;
 var guard_count = 0;
@@ -8,55 +6,78 @@ var ranged_guard_count = 0;
 module.exports = function ()
 {
 
-	var population_manager = function ()
-	{
-	};
+	for (var i in Game.creeps)
+{
+	var creep = Game.creeps[i];
 
-	population_manager.check_population = function ()
+	if (creep.memory.role == 'harvester')
 	{
-		for (var i in Game.creeps)
+		harvester_count = harvester_count + 1;
+	}
+
+	if (creep.memory.role == 'builder')
+	{
+		builder_count = builder_count + 1;
+	}
+
+	if (creep.memory.role == 'guard')
+	{
+		guard_count = guard_count + 1;
+	}
+
+	if (creep.memory.role == 'ranged_guard')
+	{
+		ranged_guard_count = ranged_guard_count + 1;
+	}
+}
+
+
+if (harvester_count <= (builder_count || guard_count || ranged_guard_count))
+{
+	for (var i in Game.spawns)
+	{
+		var possible_spawn = Game.spawns[i];
+		if (possible_spawn.canCreateCreep( [WORK, MOVE, CARRY] , undefined, {role: 'harvester'}) == OK)
 		{
-			var creep = Game.creeps[i];
-
-			if (creep.memory.role == 'harvester')
-			{
-				harvester_count = harvester_count + 1;
-			}
-
-			if (creep.memory.role == 'builder')
-			{
-				builder_count = builder_count + 1;
-			}
-
-			if (creep.memory.role == 'guard')
-			{
-				guard_count = guard_count + 1;
-			}
-
-			if (creep.memory.role == 'ranged_guard')
-			{
-				ranged_guard_count = ranged_guard_count + 1;
-			}
+			possible_spawn.createCreep( [WORK, MOVE, CARRY] , undefined, {role: 'harvester'});
 		}
-	};
+	}
+}
 
-	population_manager.balance_population = function ()
+if (guard_count < harvester_count)
+{
+	for (var i in Game.spawns)
 	{
-
-		if (harvester_count <= (builder_count || guard_count || ranged_guard_count))
+		var possible_spawn = Game.spawns[i];
+		if (possible_spawn.canCreateCreep( [TOUGH, ATTACK, MOVE, CARRY] , undefined, {role: 'guard'}) == OK)
 		{
-			//var possible_spawn = room.find(FIND_MY_SPAWNS);
-
-			for (var i in Game.spawns)
-			{
-				var possible_spawn = Game.spawns[i];
-				if (possible_spawn.canCreateCreep( [WORK, MOVE, CARRY] , undefined, {role: 'harvester'}) == OK)
-				{
-					possible_spawn.createCreep( [WORK, MOVE, CARRY] , undefined, {role: 'harvester'});
-				}
-			}
+			possible_spawn.createCreep( [WORK, MOVE, CARRY] , undefined, {role: 'guard'});
 		}
-	};
+	}
+}
 
+if (builder_count < harvester_count)
+{
+	for (var i in Game.spawns)
+	{
+		var possible_spawn = Game.spawns[i];
+		if (possible_spawn.canCreateCreep( [WORK, WORK, MOVE, CARRY] , undefined, {role: 'builder'}) == OK)
+		{
+			possible_spawn.createCreep( [WORK, WORK, MOVE, CARRY] , undefined, {role: 'builder'});
+		}
+	}
+}
+
+if (ranged_guard_count < harvester_count)
+{
+	for (var i in Game.spawns)
+	{
+		var possible_spawn = Game.spawns[i];
+		if (possible_spawn.canCreateCreep( [RANGED_ATTACK, MOVE, MOVE, TOUGH] , undefined, {role: 'ranged_guard'}) == OK)
+		{
+			possible_spawn.createCreep( [RANGED_ATTACK, MOVE, MOVE, TOUGH] , undefined, {role: 'ranged_guard'});
+		}
+	}
+}
 
 };
